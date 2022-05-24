d3713c0ec86cfa13e0d62a11108f7b514810cd2b
小马哥 <1154699897@qq.com>
2022-05-24 17:47:01 +0800

    update

---
+++ src/Providers/PayServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Magein\Payment\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Magein\Common\Commands\MakeModel;
+use Magein\Common\Commands\MakeModelProperty;
+
+/**
+ * 参考地址 https://learnku.com/laravel/t/35930
+ */
+class PayServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if (is_file(__DIR__ . '/../Common.php')) {
+            require_once __DIR__ . '/../Common.php';
+        }
+
+        foreach (glob(app_path('Helpers') . '/*.php') as $file) {
+            require_once $file;
+        }
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // 加载命令
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                MakeModel::class,
+                MakeModelProperty::class
+            ]);
+        }
+    }
+}
